dcf38293d4656833724737f6af12704bdf464372
srimani-programmer <srimani.scholar@gmail.com>
2020-05-15 17:47:09 +0530

    Code Implementation Done.

---
+++ Chapter 05/Environment Variable Creation.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    char *variable_name;
+    printf("Enter the variable to create:");
+    scanf("%s",variable_name);
+    // Returns 0 --> On Success || -1 on failure
+    int status = putenv(variable_name);
+    if(status == 0){
+        printf("Environment Variable Created Successfully.!\n");
+    }else{
+        printf("Unable to create a Environment variable.\n");
+    }
+
+    return 0;
+}
+
