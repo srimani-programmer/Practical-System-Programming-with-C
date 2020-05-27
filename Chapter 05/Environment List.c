2690a325f2a85f888c97a05514f39ae2c620966d
srimani-programmer <srimani.scholar@gmail.com>
2020-05-27 18:52:06 +0530

    Updated Fixes.

--- Chapter 05/Environment List.c
+++ Chapter 05/Environment List.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
-/* This code Helps us to prints the all the 
- Environments available in the operating system.
-*/
 int main(){
 
     extern char **environ;
+
     char **environment_list = environ;
 
+    /* This code Helps us to prints the all the 
+       Environments available in the operating system.
+    */
     while(*environment_list != NULL){
         printf("%s\n", *environment_list);
         environment_list++;
