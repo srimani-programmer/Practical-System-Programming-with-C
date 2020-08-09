c211b40e65a0c456cf9aca42ddb8d6431fbcc425
srimani-programmer <srimani.scholar@gmail.com>
2020-08-09 15:41:10 +0530

    Bug Fix #01.

--- Chapter 05/exit.c
+++ Chapter 05/exit.c
@@ -3,10 +3,20 @@
 #include<unistd.h>
 
 int main(){
-    // Prints the Current Running Process ID.
-    printf("Current Process  ID: %d\n", getpid());
-    exit(0);
-    printf("This line will not execute.\n");
+    
+    int pid = fork();
+
+    if(pid == 0){
+        // Prints the Child Process ID.
+        printf("Child Process ID: %d\n", getpid());
+        exit(0);
+    }else{
+        // Prints the Parent Process ID.
+        printf("Parent Process Id: %d\n", getppid());
+        exit(0);
+    }
+
+    printf("Processes are exited and this line will not print\n");
 
     return 0;
 }
