8474e5aac1d187813b9c4a31f762d51683682a90
srimani-programmer <srimani.scholar@gmail.com>
2020-08-12 15:38:04 +0530

    Environment variable creation using setenv.

--- Chapter 05/Environment Variable Creation.c
+++ Chapter 05/Environment Variable Creation.c
@@ -3,18 +3,27 @@
 
 int main(){
 
-    char *variable_name;
-    printf("Enter the variable to create:");
-    scanf("%s",variable_name);
+    char variable_name[15];
+    char variable_value[255];
+    int overwrittenValue;
+    printf("Enter your Variable name:");
+    scanf("%s", variable_name);
+    printf("Enter the Variable Value: ");
+    scanf("%s", variable_value);
+    
+    // 1 ---> Represents the Overridden of Value.
+    // 0 ---> Doesn't override the value
+    printf("Enter the Overridden Value: ");
+    scanf("%d", &overwrittenValue);
+
     // Returns 0 --> On Success || -1 on failure
-    int status = putenv(variable_name);
+    int status = setenv(variable_name, variable_value, overwrittenValue);
+
     if(status == 0){
-        printf("Environment Variable Created Successfully.!\n");
-    }else{
-        printf("Unable to create a Environment variable.\n");
+        printf("Environment variable Created Successfully.!\n");
+    }else if(status == -1){
+        printf("Environment variable Created Successfully.!\n");
     }
 
-    printf("%s\n", getenv(variable_name));
     return 0;
 }
-
