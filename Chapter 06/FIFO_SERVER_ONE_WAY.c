c7349c17830ff499bb9cc4f21343f5e6617034e5
srimani-programmer <srimani.scholar@gmail.com>
2020-08-17 16:32:41 +0530

    Modified the Code #01.

--- Chapter 06/FIFO_SERVER_ONE_WAY.c
+++ Chapter 06/FIFO_SERVER_ONE_WAY.c
@@ -6,29 +6,36 @@
 #include <fcntl.h>
 
 
-#define FIFO_FILE "MYFIFO"
+#define FIFO_FILE "FIFO"
+
+int file_descriptor;
+char message_buffer[1024];
+int read_buffer_bytes;
+
+void receiveData(){
+
+   while(1) {
+
+      file_descriptor = open(FIFO_FILE, O_RDONLY);
+      read_buffer_bytes = read(file_descriptor, message_buffer, sizeof(message_buffer));
+      message_buffer[read_buffer_bytes] = '\0';
+      
+      if((int)strlen(message_buffer) == 0){
+         close(file_descriptor);
+         break;
+      }
+      
+      printf("Received Message: %s\n", message_buffer);
+   }
+}
 
 int main() {
-   int fd;
-   char readbuf[80];
-   char end[10];
-   int to_end;
-   int read_bytes;
    
    // Create the FIFO if it does not exist
    mknod(FIFO_FILE, S_IFIFO|0640, 0);
-   strcpy(end, "quit");
 
-   while(1) {
-      fd = open(FIFO_FILE, O_RDONLY);
-      read_bytes = read(fd, readbuf, sizeof(readbuf));
-      readbuf[read_bytes] = '\0';
-      printf("Received Message: \"%s\" and Message length is %d\n", readbuf, (int)strlen(readbuf));
-      to_end = strcmp(readbuf, end);
-      if (to_end == 0) {
-         close(fd);
-         break;
-      }
-   }
+   // Function to receive the data from pipe.
+   receiveData();
+
    return 0;
 }
