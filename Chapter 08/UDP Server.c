737b0f0317569e1e26ef0655f676b03b85ad9e00
srimani-programmer <srimani.scholar@gmail.com>
2020-08-18 13:22:16 +0530

    Re-Implemented the UDP Server.

--- Chapter 08/UDP Server.c
+++ Chapter 08/UDP Server.c
@@ -1,4 +1,5 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include<stdlib.h> 
 #include<unistd.h>
 #include <strings.h> 
 #include <sys/types.h> 
@@ -6,40 +7,61 @@
 #include <sys/socket.h> 
 #include<netinet/in.h> 
 
-#define MAX 1024 // Maximum number of data that can transfer
-#define PORT 8080 // port number to connect
+#define MESSAGE_BUFFER 4096 // Maximum number of bytes that can transfer and receive.
+#define PORT 8888 // port number to connect
 #define SA struct sockaddr // Creating Macro for the socketaddr as SA
   
-// Driver code 
+// Global Data
+char buffer[MESSAGE_BUFFER]; 
+char message[MESSAGE_BUFFER]; 
+int socket_file_descriptor, message_size;
+socklen_t length; 
+const char *end_string = "end";
+int quit_status;
+struct sockaddr_in serveraddress, client; 
+
+void processRequest(){
+    
+    // Server Properties
+    bzero(&serveraddress, sizeof(serveraddress)); 
+    // Create a UDP Socket via PORT 8888
+    socket_file_descriptor = socket(AF_INET, SOCK_DGRAM, 0);         
+    serveraddress.sin_addr.s_addr = htonl(INADDR_ANY); 
+    // Connecting via port 8888
+    serveraddress.sin_port = htons(PORT); 
+    // Protocol Family
+    serveraddress.sin_family = AF_INET;  
+    // bind server address to socket descriptor 
+    bind(socket_file_descriptor, (SA*)&serveraddress, sizeof(serveraddress)); 
+    
+    while(1){
+        // Calculating the Client Datagram length
+        length = sizeof(client); 
+        message_size = recvfrom(socket_file_descriptor, buffer, sizeof(buffer), 0, (SA*)&client,&length);
+
+        buffer[message_size] = '\0'; 
+        quit_status = strcmp(buffer, end_string);
+
+        if(quit_status == 0){
+            printf("Server is Quitting\n");
+            close(socket_file_descriptor);
+            exit(0);
+        }
+        printf("Message Received from Client: %s\n",buffer); 
+        // sending the response to the client
+        printf("Enter reply message to the client: ");
+        scanf("%[^\n]%*c", message);
+        sendto(socket_file_descriptor, message, MESSAGE_BUFFER, 0, (SA*)&client, sizeof(client)); 
+        printf("Message Sent Succesfully to the client: %s\n", message);
+        printf("Waiting for the Reply from Client..!\n");
+    }
+    // Closing the Socket File Descriptor.
+    close(socket_file_descriptor);
+}
 int main() {   
 
-    char buffer[100]; 
-    char *message = "Greetings from Server"; 
-    int sockfd;
-    socklen_t len; 
-    struct sockaddr_in servaddr, cli; 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // Create a UDP Socket 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);         
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_family = AF_INET;  
-   
-    // bind server address to socket descriptor 
-    bind(sockfd, (SA*)&servaddr, sizeof(servaddr)); 
-       
-    //receive the datagram 
-    len = sizeof(cli); 
-    int n = recvfrom(sockfd, buffer, sizeof(buffer), 0, (SA*)&cli,&len);
-   //receive message from server 
-    buffer[n] = '\0'; 
-    puts(buffer); 
-           
-    // send the response 
-    sendto(sockfd, message, MAX, 0, (SA*)&cli, sizeof(cli)); 
-
-    close(sockfd);
-
+    printf("SERVER IS LISTENING THROUGH THE PORT: %d WITHIN A LOCAL SYSTEM\n", PORT);
+    // Calling the process request function to process the client request and give the response.
+    processRequest();
     return 0;
 }
