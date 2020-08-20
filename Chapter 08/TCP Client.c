00c6b37464c406a176db3a976caf668507253955
srimani-programmer <srimani.scholar@gmail.com>
2020-08-20 22:06:25 +0530

    Re-Implemented the TCP Client Code.

--- Chapter 08/TCP Client.c
+++ Chapter 08/TCP Client.c
@@ -1,88 +1,80 @@
-// Client-Server Appication Client Code.
-#include <stdio.h> 
-#include <stdlib.h> 
+#include<stdio.h> 
+#include<stdlib.h> 
 #include<unistd.h>
-#include <string.h> 
-#include <sys/socket.h> 
-#include <netdb.h> 
-#include <arpa/inet.h>
+#include<string.h> 
+#include<sys/socket.h> 
+#include<netdb.h> 
+#include<arpa/inet.h>
 
-#define MAX 1024 // Maximum number of data that can transfer
-#define PORT 8080 // port number to connect
+#define MESSAGE_LENGTH 1024 // Maximum number of data that can transfer
+#define PORT 8888 // port number to connect
 #define SA struct sockaddr // Creating Macro for the socketaddr as SA
 
-// Function designed for chat between client and server. 
-void communicate(int sockfd) { 
-    char buff[MAX]; 
-    int n; 
-    while(1){ 
+// Global Data
+int socket_file_descriptor, connection;
+struct sockaddr_in serveraddress, client;
+char message[MESSAGE_LENGTH];
 
-        bzero(buff, sizeof(buff)); 
-        printf("Enter the message : "); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ;
+int main(){
+
+    // Socket Creation
+    socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_file_descriptor == -1){
+        printf("Creation of Socket failed.!\n");
+        exit(1);
+    }
+
+    // Erases the memory
+    bzero(&serveraddress, sizeof(serveraddress));
+
+    // Setting the Server Properties
+    serveraddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // Setting the port numeber
+    serveraddress.sin_port = htons(PORT);
+    // Protocol family
+    serveraddress.sin_family = AF_INET;
+
+    // Establishing the Connection with server
+    connection = connect(socket_file_descriptor, (SA*)&serveraddress, sizeof(serveraddress));
+
+    if(connection == -1){
+        printf("Connection with the server failed.!\n");
+        exit(1);
+    }
+
+    // Interacting with the server
+    
+    while(1){
+
+        bzero(message, sizeof(message));
         
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            char message[4] = "exit";
-            write(sockfd, message, sizeof(message)); 
+        printf("Enter the message you want to send to the server: ");
+        scanf("%[^\n]%*c", message);
+
+         if ((strncmp(message, "end", 3)) == 0) { 
+            write(socket_file_descriptor, message, sizeof(message)); 
             printf("Client Exit.\n"); 
             break; 
-        } 
+        }
 
-        if ((strncmp(buff, "quit", 4)) == 0) { 
-            char message[4] = "quit";
-            write(sockfd, message, sizeof(message)); 
-            printf("Client Exit.\n"); 
-            break; 
-        } 
+        // Sending the data to the server by storing the number of bytes transferred in bytes variable 
+        ssize_t bytes = write(socket_file_descriptor, message, sizeof(message));
+        
+        // If the number of bytes is >= 0 then the data is sent successfully
+        if(bytes >= 0){
+            printf("Data send to the server succesfully.!\n");
+        }
 
-        write(sockfd, buff, sizeof(buff)); 
-        bzero(buff, sizeof(buff)); 
-        read(sockfd, buff, sizeof(buff)); 
-        printf("From Server : %s", buff); 
+        bzero(message, sizeof(message));
 
-        // Exit Condition for Client
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit.\n"); 
-            break; 
-        } 
+        // Reading the response from the server.
+        read(socket_file_descriptor, message, sizeof(message));
 
-        if ((strncmp(buff, "quit", 4)) == 0) { 
-            printf("Client Exit.\n"); 
-            break; 
-        } 
-    } 
-} 
-  
-int main() { 
+        printf("Data received from server: %s\n", message);
 
-    int sockfd, connfd; 
-    struct sockaddr_in servaddr, cli; 
-  
-    // socket creation and verification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
+    }
+    // Closing the Socket Connection
+    close(socket_file_descriptor);
 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // Assigning IPAddress, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-    servaddr.sin_port = htons(PORT); 
-  
-    // connect the client socket to server socket 
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-        printf("connection with the server failed...\n"); 
-        exit(0); 
-    } 
-  
-    // function for chat 
-    communicate(sockfd); 
-  
-    // close the socket 
-    close(sockfd); 
-} 
+    return 0;
+}
